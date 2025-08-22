4b27d8fd87a2bdeda124bfa7eaac3ed0972d6828
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-08-22 12:03:09 +0300

    Refactor code structure for improved readability and maintainability, adding Roboto font

---
+++ lib/core/constants/app_text_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static TextStyle textStyle1 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+  );
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_dictionary_app/core/constants/app_colors.dart';
+import 'package:smart_dictionary_app/core/constants/app_text_styles.dart';
 import 'package:smart_dictionary_app/providers/theme_provider.dart';
 import 'package:smart_dictionary_app/widgets/home_screen_search_textFieldWidget.dart';
 
@@ -16,7 +17,7 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: themeProvider.getIsDarkTheme
             ? AppColors.darkScaffoldColor
             : AppColors.lightCardColor,
-        title: const Text('Lexi'),
+        title: Text('Lexi', style: AppTextStyles.textStyle1),
         centerTitle: true,
         actions: [
           Padding(
@@ -29,6 +30,7 @@ class HomeScreen extends StatelessWidget {
         children: [
           SizedBox(height: 12),
           HomeScreenSearchTextFieldWidget(),
+         
         ],
       ),
     );
