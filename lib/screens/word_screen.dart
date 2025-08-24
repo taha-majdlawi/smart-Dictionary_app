4366023ca94a2e41264a7aa34e1fc299954e44e2
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-08-24 13:52:05 +0300

    feat: enhance WordScreen layout and add action buttons

--- lib/screens/word_screen.dart
+++ lib/screens/word_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:iconly/iconly.dart';
 import 'package:smart_dictionary_app/core/constants/app_text_styles.dart';
 import 'package:smart_dictionary_app/widgets/antonyms_words_widget.dart';
 import 'package:smart_dictionary_app/widgets/example_sentence.dart';
 import 'package:smart_dictionary_app/widgets/pronunciation_word_widget.dart';
-import 'package:smart_dictionary_app/widgets/recent_search_words_widget.dart';
 import 'package:smart_dictionary_app/widgets/synonyms_words_widget.dart';
 import 'package:smart_dictionary_app/widgets/word_defintion_widget.dart';
 
@@ -31,21 +29,102 @@ class WordScreen extends StatelessWidget {
               SizedBox(height: 28),
               SynonymsWordsWidget(),
 
-              SizedBox(height: 28),
+              SizedBox(height: 32),
 
               Text('Antonyms', style: AppTextStyles.textStyle2),
               SizedBox(height: 28),
               AntonymsWordsWidget(),
 
-              SizedBox(height: 28),
+              SizedBox(height: 32),
               Text('Example Sentence', style: AppTextStyles.textStyle2),
               SizedBox(height: 18),
               ExampleSentenceWidget(),
 
-              SizedBox(height: 28),
+              SizedBox(height: 32),
               Text('Pronunciation', style: AppTextStyles.textStyle2),
-              SizedBox(height: 18),
+              SizedBox(height: 12),
               PronunciationWidget(),
+              SizedBox(height: 32),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF1E1E1E),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 18.0,
+                          vertical: 12,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Explain with ai',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF1E1E1E),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 18.0,
+                          vertical: 12,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Add to favorites',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 12),
+              Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF1E1E1E),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 18.0,
+                      vertical: 12,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Explain with ai',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontFamily: 'Roboto',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -53,4 +132,3 @@ class WordScreen extends StatelessWidget {
     );
   }
 }
-
